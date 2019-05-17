fbfdeba2f90236ad9fb29ae370b2be92021e0ee4
ken8waru1 <ken8waru1@gmail.com>
2019-05-16 21:30:34 -0700

    Add Map class

--- W1D5/diy_adts.rb
+++ W1D5/diy_adts.rb
@@ -33,3 +33,45 @@ class Queue
     @queue.first
   end
 end
+
+class Map
+  def initialize
+    @my_map = []
+  end
+
+  def set(key, value)
+    if @my_map.any? { |pair| pair.include?(key) }
+      @my_map.each_with_index do |pair, idx_1|
+        pair.each do |ele|
+          @my_map[idx_1][1] = value 
+        end
+      end
+    else
+      @my_map << [key, value]
+    end
+  end
+
+  def get(key)
+    @my_map.each do |pair|
+      pair.each_with_index do |ele, i|
+        if ele == key && i == 0
+          return pair
+        end
+      end
+    end
+  end
+
+  def delete(key)
+    @my_map.each do |pair|
+      pair.each_with_index do |ele, i|
+        if ele == key && i == 0
+          @my_map.delete(pair)
+        end
+      end
+    end
+  end
+
+  def show
+    @my_map
+  end
+end
