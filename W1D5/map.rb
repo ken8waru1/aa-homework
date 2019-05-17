70c0ead279752d771e15809f58336bd1944a6c3a
ken8waru1 <ken8waru1@gmail.com>
2019-05-16 21:37:00 -0700

    Separate classes into individual files

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,39 +1,3 @@
-class Stack
-  def initialize
-    stack = []
-  end
-
-  def push(el)
-    stack.push(el)
-  end
-
-  def pop
-    stack.pop
-  end
-
-  def peek
-    stack.last
-  end
-end
-
-class Queue
-  def initialize
-    @queue = []
-  end
-
-  def enqueue(el)
-    @queue.push(el)
-  end
-
-  def dequeue
-    @queue.shift
-  end
-
-  def peek
-    @queue.first
-  end
-end
-
 class Map
   def initialize
     @my_map = []
