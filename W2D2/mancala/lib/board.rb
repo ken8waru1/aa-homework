d3494a1dd56a75d1d396d3347ecbbc60d4b2000a
ken8waru1 <ken8waru1@gmail.com>
2019-05-20 21:14:07 -0700

    Create cups ivar, and set up valid_move? and make_move methods

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -2,6 +2,13 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }.each.with_index do |cup, i|
+      unless i == 6 || i == 13
+        4.times { cup << :stone } 
+      end
+    end
   end
 
   def place_stones
@@ -9,9 +16,29 @@ class Board
   end
 
   def valid_move?(start_pos)
+    if @cups[start_pos].nil?
+      raise ArgumentError.new "Invalid starting cup"
+    elsif @cups[start_pos].empty?
+      raise ArgumentError.new "Starting cup is empty"
+    end
+
+    true
   end
 
   def make_move(start_pos, current_player_name)
+
+    i = start_pos
+    until @cups[start_pos].empty?
+      i += 1
+      i = 0 if i == 13
+      if i == 6 && current_player_name == @name1
+        @cups[6] << @cups[start_pos].shift
+      elsif i == 13 && current_player_name == @name2
+        @cups[13] << @cups[start_pos].shift
+      else
+        @cups[i] << @cups[start_pos].shift
+      end
+    end 
   end
 
   def next_turn(ending_cup_idx)
