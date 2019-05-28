03dcd76e812b7a2767026dbebea98b6e201bbb9a
ken8waru1 <ken8waru1@gmail.com>
2019-05-27 22:19:20 -0700

    Add Playwright class

--- W3D2/plays-playwrights-orm/plays.rb
+++ W3D2/plays-playwrights-orm/plays.rb
@@ -27,7 +27,7 @@ class Play
   end
 
   def create
-    raise "#{self} already in database" if self.id
+    raise "#{self} already in database" if self.id 
     PlayDBConnection.instance.execute(<<-SQL, self.title, self.year, self.playwright_id)
       INSERT INTO
         plays (title, year, playwright_id)
@@ -48,4 +48,55 @@ class Play
         id = ?
     SQL
   end
+
+  def self.find_by_title(title)
+    result = (PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        * 
+      FROM
+        plays
+      WHERE
+        title = ?
+      SQL
+    )
+
+    Play.new(result.first)
+  end
+end
+
+class Playwright
+  def self.all
+    data = PlayDBConnection.instance.excute("SELECT * FROM playwrights")
+    data.map { |datum| Play.new(datum) } 
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUE
+        (?, ?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.excute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        title = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
 end
