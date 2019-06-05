3375d76e2770fb917175c3b97b03083d9e0c0525
ken8waru1 <ken8waru1@gmail.com>
2019-06-04 22:46:53 -0700

    Add User model and validations

---
+++ W4D3/rails_auth/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validate :blank_password
+end
